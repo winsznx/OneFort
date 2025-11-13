e95b589682394815007372675ba8f8da6ad514c2
winsznx <timjosh507@gmail.com>
2025-11-13 17:58:37 +0100

    feat: add backend configuration

---
+++ backend/drizzle.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  schema: './src/database/schema',
+  out: './drizzle',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+} satisfies Config;
